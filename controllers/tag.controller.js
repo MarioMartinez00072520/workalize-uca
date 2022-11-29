059487f425875bb4d1ddfdceff89affb22a34ce0
MarioMartinez00072520 <00072520@uca.edu.sv>
2022-11-29 01:58:14 -0600

    Update: tag model and derivatives flow v3

--- controllers/tag.controller.js
+++ controllers/tag.controller.js
@@ -9,12 +9,6 @@ controller.create = async (req,res) => {
         const { name } = req.body;
         const {_id: userId} = req.user;
 
-        const _tag = await tag.findOne({ name: name });
-
-        if(_tag){
-            return res.status(409).json({error: "Esta etiqueta ya existe en la base de datos."})
-        }
-        
         const tag = new Tag({
             name: name,
             user: userId
@@ -24,7 +18,7 @@ controller.create = async (req,res) => {
 
         if(!newTag) return res.status(409).json({ error: "Error al crear una nueva etiqueta"})
 
-        return res.status(201).json({message: "Se ha agregado una nueva etiqueta."})
+        return res.status(201).json({newTag})
     } catch (error) {
         debug({error});
     return res.status(500).json({error: "Error inesperado de servidor."})
--- validators/tag.validators.js
+++ validators/tag.validators.js
@@ -1,11 +1,6 @@
 const { body, param } = require("express-validator");
 const validators = {};
 
-validators.createTagValidator = [
-    body("name")
-        .isEmpty().withMessage("El nombre de la etiqueta no puede ir vacio")
-        .isLength({max: 16}).withMessage("El nombre de la etiqueta no debe exceder los 16 caracteres.")
-];
 
 validators.findTagByNameValidator = [
     param("name")
